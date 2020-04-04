580de8b65b14f68b1f5df5c3923c3acfe6f56908
Joost Kersjes <joostkersjes@live.nl>
2020-04-04 13:45:10 +0200

    Basic lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Log from "./Log";
-import Timer from "./Timer";
+
+const Log = lazy(() => import("./Log"));
+const Timer = lazy(() => import("./Timer"));
 
 const App: React.FC = () => (
   <Router>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Timer</Link>
-        </li>
-        <li>
-          <Link to="/log">Log</Link>
-        </li>
-      </ul>
-    </nav>
-    <Switch>
-      <Route exact path="/">
-        <Timer />
-      </Route>
-      <Route path="/log">
-        <Log />
-      </Route>
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Timer</Link>
+          </li>
+          <li>
+            <Link to="/log">Log</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Timer />
+        </Route>
+        <Route path="/log">
+          <Log />
+        </Route>
+      </Switch>
+    </Suspense>
   </Router>
 );
 
