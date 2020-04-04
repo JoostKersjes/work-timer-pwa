be2e94764d8cadb5fb78b37816876c618096f1cd
Joost Kersjes <joostkersjes@live.nl>
2020-04-04 23:47:08 +0200

    Added working routing with Tabs

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,32 +1,36 @@
 import React, { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route, useHistory } from 'react-router-dom';
+import Tabs from 'antd/es/tabs';
 
 const Log = lazy(() => import('../pages/Log/Log'));
 const Timer = lazy(() => import('../pages/Timer/Timer'));
 
-const App: React.FC = () => (
-  <Router>
-    <Suspense fallback={<div>Loading...</div>}>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Timer</Link>
-          </li>
-          <li>
-            <Link to="/log">Log</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route exact path="/">
-          <Timer />
-        </Route>
-        <Route path="/log">
-          <Log />
-        </Route>
-      </Switch>
-    </Suspense>
-  </Router>
-);
+const App: React.FC = () => {
+  const history = useHistory();
+
+  return (
+    <>
+      <Tabs
+        size="large"
+        activeKey={history.location.pathname}
+        onChange={key => history.replace(key)}
+      >
+        <Tabs.TabPane tab="Timer" key="/" />
+        <Tabs.TabPane tab="Log" key="/log" />
+      </Tabs>
+
+      <Suspense fallback={<></>}>
+        <Switch>
+          <Route exact path="/">
+            <Timer />
+          </Route>
+          <Route path="/log">
+            <Log />
+          </Route>
+        </Switch>
+      </Suspense>
+    </>
+  );
+};
 
 export default App;
