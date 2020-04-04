0f13167c304ab09385b9a93adba07bc0b6efa6c8
Joost Kersjes <joostkersjes@live.nl>
2020-04-04 13:33:33 +0200

    Added basic routing solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Log from "./Log";
+import Timer from "./Timer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Timer</Link>
+        </li>
+        <li>
+          <Link to="/log">Log</Link>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route exact path="/">
+        <Timer />
+      </Route>
+      <Route path="/log">
+        <Log />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
